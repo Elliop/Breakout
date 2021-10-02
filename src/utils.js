851bfc9849784cbed4a58016e3a4393d3dc19e74
Elliop <elliop@Elliops-MacBook-Pro.local>
2021-10-02 17:50:22 +0100

    Game State from Level

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
-export default getRange = (length) => [...Array(length).keys()];
+export const getRange = (length) => {
+  [...Array(length).keys()];
+};
+
+export const getRandomFrom = (...args) => {
+  args[Math.floor(Math.random() * args.length)];
+};
+
+export const flatten = (arrays) => {
+  arrays.reduce((acc, row) => [...acc, ...row], []);
+};
