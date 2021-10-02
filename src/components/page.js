a6e7abfa6225604a824b00c816f68d1aca8551e9
Elliop <elliop@Elliops-MacBook-Pro.local>
2021-10-02 19:17:22 +0100

    Page Componen

--- src/components/page.js
+++ src/components/page.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import Screen from "./screen";
+import { registerListner } from "../utils";
 
 export default () => {
-  return <div className="page" />;
+  const screenContainer = useRef();
+  const [size, setSize] = useState();
+
+  useEffect(() => {
+    const onResize = () => {
+      const { width, height } = screenContainer.current.getBoundingClientRect();
+      setSize({ width, height });
+    };
+    const unregisterResizeListner = registerListner("resize", onResize);
+    onResize();
+    return unregisterResizeListner;
+  }, []);
+  return (
+    <div className="page">
+      <div className="screen-container" ref={screenContainer}>
+        {size && <Screen {...size} />}
+      </div>
+    </div>
+  );
 };
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,15 @@
-export const getRange = (length) => {
-  [...Array(length).keys()];
-};
+export const getRange = (length) => [...Array(length).keys()];
 
-export const getRandomFrom = (...args) => {
+export const getRandomFrom = (...args) =>
   args[Math.floor(Math.random() * args.length)];
-};
 
 export const flatten = (arrays) => {
   arrays.reduce((acc, row) => [...acc, ...row], []);
+};
+
+export const registerListner = (eventName, handler) => {
+  window.addEventListener(eventName, handler);
+  return () => {
+    window.removeEventListener(eventName, handler);
+  };
 };
