291f3a53b97d99b2ce37f9c76f0eb917f19b6919
Elliop <elliop@Elliops-MacBook-Pro.local>
2021-10-03 15:26:03 +0100

    Core Logic

--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,11 @@ export const registerListener = (eventName, handler) => {
   window.addEventListener(eventName, handler);
   return () => window.removeEventListener(eventName, handler);
 };
+
+export const toDegrees = (radians) => (radians * 180) / Math.PI;
+export const toRadians = (degrees) => (degrees * Math.PI) / 180;
+
+export const withoutElement = (array, element) =>
+  array.filter((e) => e !== element);
+export const updateElement = (array, oldElement, newElement) =>
+  array.map((e) => (e === oldElement ? newElement : e));
